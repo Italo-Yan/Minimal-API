a54d7d0abda58ac20ffbf7048df1593fc6bb82a6
Italo-Yan <italoyandev@gmail.com>
2025-11-08 18:07:47 -0300

    feat: criando a estutura do bancos de dados

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
+using MinimalAPI.Infraestrutura.DB;
+using MinimalAPI.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+  options.UseMySql(
+    builder.Configuration.GetConnectionString("mysql"),
+    ServerVersion.AutoDetect(
+      builder.Configuration.GetConnectionString("mysql")
+    )
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalAPI.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", (LoginDTO loginDTO) =>
 {
   if (loginDTO.Email == "adm@teste.com" && loginDTO.Password == "123456")
     return Results.Ok("Login realizado com sucesso!");
